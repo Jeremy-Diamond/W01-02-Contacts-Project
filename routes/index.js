13bfd59b13d942da3adeb161ad61785c7681c1e0
Jeremy Diamond <socjeremyd@gmail.com>
2023-09-16 17:51:26 -0600

    removing Hello World

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello World!');});
+router.get('/', (req, res) => {res.send('Welcome to my CIT341 Project! Please add a route to get what you are looking for.');});
 
 router.use('/contacts',require('./contacts'));
 
